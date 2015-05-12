f529f033beaaccbf15208766d7fec0aa4b38a68a
Allee Brett <alleebrett@gmail.com>
2015-05-12 14:21:59 +0200

    allee

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,1 +1,1 @@
-﻿import React from 'react';class Test extends React.Component {  render(){    return (      <h1>HELLO WORLD</h1>    )  }}React.render(<Test />, document.body);
+﻿import React from 'react';class Test extends React.Component {  render(){    return (      <h1>HELLO Allee</h1>    )  }}React.render(<Test />, document.body);
